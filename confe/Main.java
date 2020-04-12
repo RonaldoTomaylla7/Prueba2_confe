cd1ee9b6bfde1572c26920eff9e618bb1e1a7629
Ronaldo Tomaylla <ronaldo_11_08@hotmail.com>
2020-04-12 17:32:47 -0500

    CHUPALAAA COMMIT

---
+++ confe/Main.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package confe;
+
+/**
+ *
+ * @author Ronaldo Tomaylla Marcelo , CODIGO: 20150938
+ *
+ */
+
+
+
+import java.io.File;
+import java.util.*;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("bebe");
+        String cadena;
+        
+        
+        int n;
+    System.out.print("Introduzca un número entero: ");
+    n = sc.nextInt();
+
+       /* List lista = new ArrayList();
+        Scanner reader = new Scanner(System.in);
+        int numero = 0;
+
+        System.out.println("Introduce números. El cero para salir");
+
+        do {
+            try {
+                numero = reader.nextInt();
+                lista.add(numero);
+            } catch (InputMismatchException ime) {
+                System.out.println("¡Cuidado! Solo puedes insertar números. ");
+                reader.next();
+            }
+        } while (numero != 0);
+            */
+       
+    }
+
+}
---
+++ confe/prin.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Ronaldo Tomaylla Marcelo , CODIGO: 20150938
+ *
+ */
+
+
+
+
+import java.util.Scanner;
+
+public class prin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("bebe");
+        String cadena;
+        
+        
+        int n;
+            System.out.print("Introduzca un número entero: ");
+             n = sc.nextInt();
+
+       /* List lista = new ArrayList();
+        Scanner reader = new Scanner(System.in);
+        int numero = 0;
+
+        System.out.println("Introduce números. El cero para salir");
+
+        do {
+            try {
+                numero = reader.nextInt();
+                lista.add(numero);
+            } catch (InputMismatchException ime) {
+                System.out.println("¡Cuidado! Solo puedes insertar números. ");
+                reader.next();
+            }
+        } while (numero != 0);
+            */
+       
+    }
+
+}
